277ffebd5181762c10947302dcd037d5df43b994
MADHAVAN-2004 <97903056+MADHAVAN-2004@users.noreply.github.com>
2022-01-20 07:56:55 -0800

    Add files via upload

---
+++ let's upgrade assignmet python day 4.py
@@ -0,0 +1,37 @@
+a=1
+b=1.5
+c='a'
+d="let us upgrade"
+e=[1,2,3,4,5]
+f=(1,2,3,4,5)
+g={"name":"LET US UPGRADE","AGE":11}
+def function_1(a,b,c,d,e,f,g):
+	print(f"the value of a is:{a}")
+	print(f"the value of b is:{b}")
+	print(f"the value of c is:{c}")
+	print(f"the value of d is:{d}")
+	print(f"the value of e is:{e}")
+	print(f"the value of f is:{f}")
+	print(f"the value of g is:{g}")
+v=[1,2,3,4,5]
+def function_2(v):
+		for i in v:
+			print(i, end="\t")
+x=1
+y=2
+empty=[]
+def function_3(x,y):
+	empty.append(x)
+	empty.append(y)
+	print("\n",empty)
+function_1(a,b,c,d,e,f,g)
+function_2(v)
+function_3(x,y)	
+			
+	
+	
+	
+	
+		
+		
+		
